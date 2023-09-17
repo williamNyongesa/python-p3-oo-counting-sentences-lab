fec1993dd83cd884a5129a35492b45d40b041874
William Nyongesa <marakawilliam@gmail.com>
2023-09-17 10:24:15 +0300

    completed partially

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        sentences = re.split(r'[.!?]', self.value)
+        # Filter out empty strings and whitespace-only strings
+        sentences = [s.strip() for s in sentences if s.strip()]
+        return len(sentences)
